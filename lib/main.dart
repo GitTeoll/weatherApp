dd663349582439d33f32b473984a2689433e698d
Lang Kim <mrlangkim@gmail.com>
2025-11-29 20:22:32 +0900

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+// branch - feature/weather-app
 
 void main() {
   runApp(ProviderScope(child: WeatherApp()));
